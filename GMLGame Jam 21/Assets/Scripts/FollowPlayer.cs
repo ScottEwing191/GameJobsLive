a4eb764eeba2fac0be11d4a4f3cde53a6bb359ed
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-07-12 18:44:04 +0100

    Added CharacterController

---
+++ GMLGame Jam 21/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private bool shouldFollowPlayer = true;
+    [SerializeField] private Transform player;
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;	// How much to smooth out the movement
+    
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    private void Awake() {
+        if (player!= null) {
+            offset = transform.position - player.position;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (shouldFollowPlayer) {
+            
+            Vector3 currentVelocity = Vector3.zero;
+            transform.position = Vector3.SmoothDamp(transform.position, player.position + offset, ref currentVelocity, movementSmoothing);
+        }
+    }
+
+
+}
---
+++ GMLGame Jam 21/Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour
+{
+
+    [SerializeField] private CharacterController2D controller;
+    [SerializeField] private float runSpeed = 40f;
+    [SerializeField] private float resetTime = 1f;      // the time the reset button is held for before the level resets
+    [SerializeField] bool shouldLimitKeyPresses = false;
+    
+    private bool hasReleasedRight = false;
+    private bool hasReleasedLeft = false;
+    private bool hasPressedJump = false;
+
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+
+    private Coroutine resetTimer;
+    private bool isResetTimerRoutineRunning = false;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Get Left Input
+        if (hasReleasedLeft == false) {
+            horizontalMove = Input.GetAxisRaw("HorizontalLeft") * runSpeed; 
+        }
+        else { horizontalMove = 0;}
+        if (Input.GetButtonUp("HorizontalLeft") && shouldLimitKeyPresses) {
+            hasReleasedLeft = true;
+        }
+
+        // Get Right Input
+        if (hasReleasedRight == false) {
+            horizontalMove += Input.GetAxisRaw("HorizontalRight") * runSpeed;
+        }
+        else { horizontalMove += 0; }
+        if (Input.GetButtonUp("HorizontalRight") && shouldLimitKeyPresses) {
+            hasReleasedRight = true;
+        }
+        
+        // Jump Input
+        if (Input.GetButtonDown("Jump") && !hasPressedJump && shouldLimitKeyPresses)
+        {
+            jump = true;
+            hasPressedJump = true;
+        }
+        // Reset Input
+        if (Input.GetButtonDown("Reset")) {
+            resetTimer = StartCoroutine(ResetTimer());
+        }
+        if (Input.GetButtonUp("Reset") && isResetTimerRoutineRunning) {
+            StopCoroutine(resetTimer);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime,false, jump);
+        jump = false;
+    }
+
+    IEnumerator ResetTimer() {
+        isResetTimerRoutineRunning = true;
+        yield return new WaitForSeconds(resetTime);
+        ResetPlayer();
+        isResetTimerRoutineRunning = false;
+    }
+
+    private void ResetPlayer() {
+        controller.ResetPosition();
+        ResetInputs();
+    }
+    public void ResetInputs() {
+        hasReleasedLeft = false;
+        hasReleasedRight = false;
+        hasPressedJump = false;
+    }
+}
