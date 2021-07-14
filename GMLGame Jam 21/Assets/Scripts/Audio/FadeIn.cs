4323ba0a396953b24c89264fdfda3cbf72bebe6c
Miroslaw Sienkiewicz <32368860+BattleAngelSound@users.noreply.github.com>
2021-07-14 16:49:42 +0200

    Music background Added

---
+++ GMLGame Jam 21/Assets/Scripts/Audio/FadeIn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class FadeIn : MonoBehaviour
+{
+    [SerializeField]
+    private int fadeInTime = 3;
+    private AudioSource audioSource;
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    private void Update()
+    {
+        if (audioSource.volume < 1)
+        {
+            audioSource.volume = audioSource.volume + (Time.deltaTime / (fadeInTime + 1));
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
