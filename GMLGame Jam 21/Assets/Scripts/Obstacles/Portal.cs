71714e58d51812c4398f3440d224ca74fc3e3481
Miroslaw Sienkiewicz <32368860+BattleAngelSound@users.noreply.github.com>
2021-07-17 16:25:29 +0200

    Portal SFX added

--- GMLGame Jam 21/Assets/Scripts/Obstacles/Portal.cs
+++ GMLGame Jam 21/Assets/Scripts/Obstacles/Portal.cs
@@ -6,10 +6,13 @@ public class Portal : MonoBehaviour
 {
 
     [SerializeField] private Transform targetPortal;
+    [SerializeField] private AudioSource portalSource;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             collision.transform.position = targetPortal.position;
+            portalSource.pitch = Random.Range(0.85f, 1.1f);
+            portalSource.Play();
         }
     }
 
