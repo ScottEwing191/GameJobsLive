39b243ddd427ab8eec05ab76bed2af6b89a2eceb
Miroslaw Sienkiewicz <32368860+BattleAngelSound@users.noreply.github.com>
2021-07-24 19:26:48 +0200

    MainMenu music added

---
+++ GMLGame Jam 21/Assets/Scripts/Audio/MusicRandomPosStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicRandomPosStart : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.time = Random.Range(0f, audioSource.clip.length);
+        audioSource.Play();
+    }
+}
