d9b1be135ab8af55f91437a2977551eaf81aa496
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-07-24 15:43:13 +0100

    Added Backup Setting Audio Menu

---
+++ GMLGame Jam 21/Assets/Scripts/Audio/SetVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVolume : MonoBehaviour
+{
+    
+    // Normally this is done in the Gamemanager script. But since the Level scene doesn't have A Game Manager It's getting done here
+    void Start()
+    {
+        Volume gameVolume = FindObjectOfType<Volume>();
+
+        if (gameVolume != null) {
+            if (!gameVolume.isVolumeOn) {
+                Camera.main.GetComponent<AudioListener>().enabled = false;
+            }
+        }
+    }
+
+   
+}
