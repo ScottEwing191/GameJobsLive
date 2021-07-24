42d3c90004229e15d2ac436cc7b885c62c9a49e9
Miroslaw Sienkiewicz <32368860+BattleAngelSound@users.noreply.github.com>
2021-07-24 17:55:14 +0200

    Fire SFX added to Portal Prefab

---
+++ GMLGame Jam 21/Assets/Scripts/Audio/RandomPosStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPosStart : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.time = Random.Range(0f, audioSource.clip.length);
+        audioSource.pitch = Random.Range(1.8f, 2.8f);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
