bc5541f71cb8e0a6031e5362665580f4965e5212
Aman Agrawal <aman9893089064@gmail.com>
2021-07-25 02:58:20 +0530

    final build

--- GMLGame Jam 21/Assets/Scripts/UI/DialogScript.cs
+++ GMLGame Jam 21/Assets/Scripts/UI/DialogScript.cs
@@ -15,12 +15,34 @@ public class DialogScript : MonoBehaviour
         StartCoroutine(Type());
     }
 
+    public void Update()
+    {
+        if (dialogText.text == sentences[index])
+        {
+            NextSentence();
+        }
+    }
+
     IEnumerator Type()
     {
         foreach (char letter in sentences[index].ToCharArray())
         {
             dialogText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            dialogText.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            dialogText.text = "";
         }
     }
 }
